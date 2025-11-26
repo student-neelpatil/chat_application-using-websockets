107b3d38ac7a0d4091517e020a8b6c12e5871763
student-neelpatil <patilneel288@gmail.com>
2025-11-26 20:16:04 +0530

    websocket server is created

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,11 +1,31 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 //creating websocket server
 function startserver() {
     const wss = new WebSocketServer({ port: 8080 });
     let usercount = 0;
+    let allsockets = [];
     wss.on("connection", (socket) => {
+        //pushing every socket to this array so when we send message it broadcast to evey user
+        allsockets.push(socket);
         usercount = usercount + 1;
         console.log("user connected", usercount);
+        //sending the message from client
+        socket.on("message", (message) => {
+            console.log(message.toString());
+            //send message from server and broadcasting to all clients
+            //   for(let i=0;i<allsockets.length;i++)
+            //   {
+            //     const s =allsockets[i];//here we get all sockets one by one
+            //     s.send(message.toString()+"sent from the server");
+            //   }
+            //send message from server and broadcasting to all clients
+            for (const s of allsockets) {
+                s.send(message.toString() + " sent from server");
+            }
+            socket.on("close", () => {
+                allsockets = allsockets.filter((x) => x != socket);
+            });
+        });
     });
 }
 startserver();
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,53 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
+
+
 
 //creating websocket server
 
-function startserver(){
+function startserver() {
 
-    const wss= new WebSocketServer({port:8080});
+    const wss = new WebSocketServer({ port: 8080 });
 
-    let usercount=0;
+    let usercount = 0;
+    let allsockets: WebSocket[] = [];
 
-    wss.on("connection",(socket)=>{
-       usercount=usercount+1;
-       console.log("user connected",usercount);
+    wss.on("connection", (socket) => {
+
+        //pushing every socket to this array so when we send message it broadcast to evey user
+        allsockets.push(socket);
+        usercount = usercount + 1;
+        console.log("user connected", usercount);
+
+        //sending the message from client
+        socket.on("message", (message) => {
+            console.log(message.toString());
+
+            //send message from server and broadcasting to all clients
+
+            //   for(let i=0;i<allsockets.length;i++)
+            //   {
+            //     const s =allsockets[i];//here we get all sockets one by one
+
+            //     s.send(message.toString()+"sent from the server");
+            //   }
+
+        //send message from server and broadcasting to all clients
+            for (const s of allsockets) {
+                s.send(message.toString() + " sent from server");
+            }
+
+
+            socket.on("close",()=>{
+                allsockets=allsockets.filter((x)=>x != socket);
+            })
+
+
+
+        })
     })
+
 }
 
 
 
-startserver();
+startserver()
